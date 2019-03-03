47b4767e75c067096fd580cf6a1b6328f85bcb7d
MANISH KUMAR <manish79kr@gmail.com>
2019-03-03 14:08:03 +0530

    Initial commit. Learning GraphView

---
+++ app/src/main/java/com/example/mrmnz/bloodglucometer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mrmnz.bloodglucometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1,3),
+                new DataPoint(2, 2),
+                new DataPoint(200, 7)
+        });
+        graph.addSeries(series);
+    }
+}
